5e9cd4c82f747b8c9fc4493f72eeab5f79da6547
divya-subramonian25 <divya.subramonian25@gmail.com>
2024-10-22 18:07:56 -0700

    ds - update RecommendationRequestRepository

--- src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/RecommendationRequestRepository.java
@@ -1,7 +1,6 @@
 package edu.ucsb.cs156.example.repositories;
 
 import edu.ucsb.cs156.example.entities.RecommendationRequest;
-import edu.ucsb.cs156.example.entities.UCSBDate;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -17,5 +16,5 @@ public interface RecommendationRequestRepository extends CrudRepository<Recommen
      * @param quarterYYYYQ quarter in the format YYYYQ (e.g. 20241 for Winter 2024, 20242 for Spring 2024, 20243 for Summer 2024, 20244 for Fall 2024)
      * @return all UCSBDate entities with a given quarterYYYYQ
      */
-    Iterable<UCSBDate> findAllByQuarterYYYYQ(String quarterYYYYQ);
+    Iterable<RecommendationRequest> findAllByQuarterYYYYQ(String quarterYYYYQ);
 }
